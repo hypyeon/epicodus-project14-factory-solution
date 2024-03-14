6ba625e07fb2c5dd104439fa6fccf2e96aa43d0d
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-14 08:04:36 -0700

    back to code - starting with MachinesController

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController: Controller
+  {
+    
+  }
+}
