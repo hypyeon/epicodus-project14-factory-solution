5b74ed0f895d05be5f52caaf69c8e80969a9db20
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-13 23:47:22 -0700

    add EngineerController and all views for Engineer

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,82 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer eng)
+    {
+      _db.Engineers.Add(eng);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Engineer eng = _db.Engineers
+        .Include(e => e.Machines)
+        .FirstOrDefault(e => e.EngineerId == id);
+      return View(eng);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Engineer eng = _db.Engineers.FirstOrDefault(e => e.EngineerId == id);
+      return View(eng);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer eng)
+    {
+      _db.Engineers.Update(eng);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = eng.EngineerId });
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Engineer eng = _db.Engineers
+        .FirstOrDefault(e => e.EngineerId == id);
+      if (eng == null)
+      {
+        return NotFound();
+      }
+      return View(eng);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteCompleted(int id)
+    {
+      Engineer eng = _db.Engineers
+        .FirstOrDefault(e => e.EngineerId == id);
+      _db.Engineers.Remove(eng);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -3,7 +3,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; }
     public string Difficulty { get; set; }
     public int EngineerId { get; set; }
     public Engineer Engineer { get; set; }
