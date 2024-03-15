d151a4928a48bc5b99731a6f1dbbfbff92cb1b0e
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-14 17:22:45 -0700

    add Controller and Index View for License

---
+++ Factory/Migrations/20240314233902_EditLicenseEntity.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class EditLicenseEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EngineerLicenses_Licneses_LicenseId",
+                table: "EngineerLicenses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Licneses",
+                table: "Licneses");
+
+            migrationBuilder.RenameTable(
+                name: "Licneses",
+                newName: "Licenses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Licenses",
+                table: "Licenses",
+                column: "LicenseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EngineerLicenses_Licenses_LicenseId",
+                table: "EngineerLicenses",
+                column: "LicenseId",
+                principalTable: "Licenses",
+                principalColumn: "LicenseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EngineerLicenses_Licenses_LicenseId",
+                table: "EngineerLicenses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Licenses",
+                table: "Licenses");
+
+            migrationBuilder.RenameTable(
+                name: "Licenses",
+                newName: "Licneses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Licneses",
+                table: "Licneses",
+                column: "LicenseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EngineerLicenses_Licneses_LicenseId",
+                table: "EngineerLicenses",
+                column: "LicenseId",
+                principalTable: "Licneses",
+                principalColumn: "LicenseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -6,7 +6,7 @@ namespace Factory.Models
   {
     public DbSet<Engineer> Engineers { get; set; }
     public DbSet<Machine> Machines { get; set; }
-    public DbSet<License> Licneses { get; set; }
+    public DbSet<License> Licenses { get; set; }
     public DbSet<EngineerLicense> EngineerLicenses { get; set; }
 
     public FactoryContext(DbContextOptions options) : base(options) { }
