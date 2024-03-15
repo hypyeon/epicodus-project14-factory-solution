bdef92bb3b53cc60fc66fc2bcb7c549ad35248e7
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-14 22:52:03 -0700

    add AddMachine and AddEngineer views

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "Name of machine should have a value.")]
     public string Name { get; set; }
     public string Difficulty { get; set; }
     public List<EngineerMachine> AssignedEngineers { get; }
